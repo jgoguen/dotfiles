d310a1aa37b01bd91537e5307c9f3ac21ae898ed
Joel Goguen <jgoguen@users.noreply.github.com>
2022-12-13 18:49:27 -0500

    Fix up Neo Tree settings

--- dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/neo-tree.lua
+++ dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/neo-tree.lua
@@ -3,7 +3,43 @@ return {
   window = {
     width = 40,
     mappings = {
-      ["i"] = "open_split",
+      ["<CR>"] = "open_with_window_picker",
+      ["a"] = {
+        "add",
+        config = {
+          show_path = "relative",
+        },
+      },
+      ["A"] = {
+        "add_directory",
+        config = {
+          show_path = "relative",
+        },
+      },
+      ["c"] = {
+        "copy",
+        config = {
+          show_path = "relative",
+        },
+      },
+      ["m"] = {
+        "move",
+        config = {
+          show_path = "relative",
+        },
+      },
+      ["i"] = "split_with_window_picker",
+      ["s"] = "vsplit_with_window_picker",
     },
+  },
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      never_show = {
+        ".DS_Store",
+        "thumbs.db",
+      },
+    },
+    use_libuv_file_watcher = true,
   },
 }
---
+++ dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/window-picker.lua
@@ -0,0 +1,12 @@
+return {
+  autoselect_one = true,
+  include_current = true,
+  filter_rules = {
+    bo = {
+      -- if the file type is one of following, the window will be ignored
+      filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+      -- if the buffer type is one of following, the window will be ignored
+      buftype = { 'terminal', "quickfix" },
+    },
+  },
+}
