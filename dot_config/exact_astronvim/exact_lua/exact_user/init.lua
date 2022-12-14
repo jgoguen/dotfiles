5088771f203bf4e4d940d28687c3eee20b9908e0
Joel Goguen <jgoguen@users.noreply.github.com>
2022-12-13 22:58:28 -0500

    Use global status line

--- dot_config/exact_astronvim/exact_lua/exact_user/init.lua
+++ dot_config/exact_astronvim/exact_lua/exact_user/init.lua
@@ -1,131 +1,143 @@
 local settings_paths = {
-	"settings/*.vim",
-	"settings/plugins/*.vim",
-	"settings/post/*.vim",
+  "settings/*.vim",
+  "settings/plugins/*.vim",
+  "settings/post/*.vim",
 }
 
 local config = {
-	-- Configure AstroNvim updates
-	updater = {
-		remote = "origin",
-		channel = "stable",
-		version = "latest",
-		pin_plugins = nil,
-		skip_prompts = false,
-		show_changelog = true,
-	},
+  -- Configure AstroNvim updates
+  updater = {
+    remote = "origin",
+    channel = "stable",
+    version = "latest",
+    pin_plugins = nil,
+    skip_prompts = false,
+    show_changelog = true,
+  },
 
-	colorscheme = 'tokyonight',
+  colorscheme = 'tokyonight',
 
-	polish = function()
-		local has_telescope, telescope = pcall(require, 'telescope')
-		if has_telescope then
-			telescope.load_extension('fzf')
-		end
+  polish = function()
+    local has_telescope, telescope = pcall(require, 'telescope')
+    if has_telescope then
+      telescope.load_extension('fzf')
+    end
 
-		require("keybindings").setup()
+    require("keybindings").setup()
+    vim.keymap.set(
+      'n',
+      '<leader>w',
+      function()
+        local has_picker, picker = pcall(require, 'window-picker')
+        if has_picker then
+          local wid = picker.pick_window() or vim.api.nvim_get_current_win()
+          vim.api.nvim_set_current_win(wid)
+        end
+      end,
+      { desc = 'Pick a window' }
+    )
 
-		-- Also scan current and included files for defined name or macro
-		vim.opt.complete:append({
-			d = true,
-		})
+    -- Also scan current and included files for defined name or macro
+    vim.opt.complete:append({
+      d = true,
+    })
 
-		-- Shorten messages
-		-- m - Use [+] instead of [modified]
-		-- r - Use [RO] instead of [readonly]
-		-- I - Don't give the :intro message when starting vim
-		vim.opt.shortmess:append({
-			m = true,
-			r = true,
-			I = true,
-		})
+    -- Shorten messages
+    -- m - Use [+] instead of [modified]
+    -- r - Use [RO] instead of [readonly]
+    -- I - Don't give the :intro message when starting vim
+    vim.opt.shortmess:append({
+      m = true,
+      r = true,
+      I = true,
+    })
 
-		-- Allow backspacing over everything in insert mode, C-w and C-u do not stop at
-		-- start of insert
-		vim.cmd([[set backspace=indent,eol,nostop]])
+    -- Allow backspacing over everything in insert mode, C-w and C-u do not stop at
+    -- start of insert
+    vim.cmd([[set backspace=indent,eol,nostop]])
 
-		-- Set a custom foldmarker to not conflict with templates
-		vim.cmd("set foldmarker=[[[,]]]")
+    -- Set a custom foldmarker to not conflict with templates
+    vim.cmd("set foldmarker=[[[,]]]")
 
-		-- listchars options [[[
-		-- tab:xy - Print x, then y as many times as possible in the tab space
-		-- trail - Character to show for trailing spaces
-		-- extends - Character to show when 'wrap' is off and line extends off-screen
-		-- precedes - Character to show when line extends left off screen
-		-- nbsp - Character to show for a non-breakable space
-		-- conceal - Character to show for concealed text when conceallevel is 1
-		-- ]]]
-		vim.cmd([[set listchars=tab:▷┅,trail:•,extends:»,precedes:«,nbsp:✖,conceal:≠]])
-		-- ]]]
+    -- listchars options [[[
+    -- tab:xy - Print x, then y as many times as possible in the tab space
+    -- trail - Character to show for trailing spaces
+    -- extends - Character to show when 'wrap' is off and line extends off-screen
+    -- precedes - Character to show when line extends left off screen
+    -- nbsp - Character to show for a non-breakable space
+    -- conceal - Character to show for concealed text when conceallevel is 1
+    -- ]]]
+    vim.cmd([[set listchars=tab:▷┅,trail:•,extends:»,precedes:«,nbsp:✖,conceal:≠]])
+    -- ]]]
 
-		-- Influence insert-mode completion
-		vim.cmd([[set completeopt=longest,menu,menuone,preview,noinsert]])
+    -- Influence insert-mode completion
+    vim.cmd([[set completeopt=longest,menu,menuone,preview,noinsert]])
 
-		-- Open diffs in vertical splits
-		vim.opt.diffopt:append({
-			vertical = true,
-		})
+    -- Open diffs in vertical splits
+    vim.opt.diffopt:append({
+      vertical = true,
+    })
 
-		-- Set characters for filling
-		vim.cmd([[set fillchars=vert:┃,fold:\ ,diff:-,eob:\ ,msgsep:‾]])
+    -- Set characters for filling
+    vim.cmd([[set fillchars=vert:┃,fold:\ ,diff:-,eob:\ ,msgsep:‾]])
 
-		-- Set formatting options [[[
-		-- t: Auto-wrap text using textwidth
-		-- c: Auto-wrap comments using textwidth, and insert the comment leader
-		-- q: Allow formatting comments with `gq`
-		-- n: Recognize numbered lists when formatting
-		-- m: Break at multibyte characters above 255
-		-- M: Don't insert a space before/after multibyte characters
-		-- 1: Break a line before a one-letter word instead of after
-		-- j: Remove a comment leader when joining lines if it makes sense
-		-- ]]]
-		vim.cmd([[set formatoptions=tcqnmM1j]])
+    -- Set formatting options [[[
+    -- t: Auto-wrap text using textwidth
+    -- c: Auto-wrap comments using textwidth, and insert the comment leader
+    -- q: Allow formatting comments with `gq`
+    -- n: Recognize numbered lists when formatting
+    -- m: Break at multibyte characters above 255
+    -- M: Don't insert a space before/after multibyte characters
+    -- 1: Break a line before a one-letter word instead of after
+    -- j: Remove a comment leader when joining lines if it makes sense
+    -- ]]]
+    vim.cmd([[set formatoptions=tcqnmM1j]])
 
-		-- Add matching pairs. % jumps between pairs
-		vim.cmd([[set matchpairs+=<:>,「:」,『:』,【:】,“:”,‘:’,《:》,':',":"]])
+    -- Add matching pairs. % jumps between pairs
+    vim.cmd([[set matchpairs+=<:>,「:」,『:』,【:】,“:”,‘:’,《:》,':',":"]])
 
-		-- Use specific regional variations of English for spelling
-		vim.cmd([[set spelllang=en_us,en_ca,en_gb]])
+    -- Use specific regional variations of English for spelling
+    vim.cmd([[set spelllang=en_us,en_ca,en_gb]])
 
-		-- Allow left/right arrows to go to the previous/next line
-		vim.cmd("set whichwrap+=<,>,[,]")
+    -- Allow left/right arrows to go to the previous/next line
+    vim.cmd("set whichwrap+=<,>,[,]")
 
-		local py3_venv = vim.fn.stdpath('data') .. '/venv/bin/python3'
-		if vim.fn.filereadable(py3_venv) then
-			vim.g.python3_host_prog = py3_venv
-		elseif vim.fn.executable('python3') then
-			vim.g.python3_host_prog = vim.fn.resolve(vim.fn.exepath('python3'))
-		else
-			error('Python 3 not found')
-		end
+    local py3_venv = vim.fn.stdpath('data') .. '/venv/bin/python3'
+    if vim.fn.filereadable(py3_venv) then
+      vim.g.python3_host_prog = py3_venv
+    elseif vim.fn.executable('python3') then
+      vim.g.python3_host_prog = vim.fn.resolve(vim.fn.exepath('python3'))
+    else
+      error('Python 3 not found')
+    end
 
-		if vim.fn.executable("rg") then
-			vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
-			vim.opt.grepformat = "%f:%l:%c:%m"
-		elseif vim.fn.executable("ag") then
-			vim.opt.grepprg = "ag --nogroup --nocolor"
-		end
+    if vim.fn.executable("rg") then
+      vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+      vim.opt.grepformat = "%f:%l:%c:%m"
+    elseif vim.fn.executable("ag") then
+      vim.opt.grepprg = "ag --nogroup --nocolor"
+    end
 
-		for _, path in ipairs(settings_paths) do
-			local settings = vim.fn.globpath(vim.o.runtimepath, path, 0, 1)
-			for _, f in ipairs(settings) do
-				vim.cmd("source " .. f)
-			end
-		end
+    for _, path in ipairs(settings_paths) do
+      local settings = vim.fn.globpath(vim.o.runtimepath, path, 0, 1)
+      for _, f in ipairs(settings) do
+        vim.cmd("source " .. f)
+      end
+    end
 
-		local xdg_config = os.getenv('XDG_CONFIG_HOME')
-		if xdg_config == nil then
-			xdg_config = os.getenv('HOME') .. '/.config'
-		end
+    local xdg_config = os.getenv('XDG_CONFIG_HOME')
+    if xdg_config == nil then
+      xdg_config = os.getenv('HOME') .. '/.config'
+    end
 
-		if vim.fn.filereadable(xdg_config .. '/vim-local.vim') ~= 0 then
-			vim.cmd('source ' .. xdg_config .. '/vim-local.vim')
-		end
+    if vim.fn.filereadable(xdg_config .. '/vim-local.vim') ~= 0 then
+      vim.cmd('source ' .. xdg_config .. '/vim-local.vim')
+    end
 
-		if vim.fn.filereadable(xdg_config .. '/vim-local.lua') ~= 0 then
-			vim.cmd('source ' .. xdg_config .. '/vim-local.lua')
-		end
-	end,
+    if vim.fn.filereadable(xdg_config .. '/vim-local.lua') ~= 0 then
+      vim.cmd('source ' .. xdg_config .. '/vim-local.lua')
+    end
+  end,
 }
 
 return config
--- dot_config/exact_astronvim/exact_lua/exact_user/options.lua
+++ dot_config/exact_astronvim/exact_lua/exact_user/options.lua
@@ -1,231 +1,231 @@
 local options = {
-	opt = {
-		-- Do not write backup or swap files [[[
-		backup = false,
-		swapfile = false,
-		writebackup = false,
-		-- ]]]
+  opt = {
+    -- Do not write backup or swap files [[[
+    backup = false,
+    swapfile = false,
+    writebackup = false,
+    -- ]]]
 
-		-- Set intelligent case insensitivity when searching [[[
-		ignorecase = true,
-		smartcase = true,
-		tagcase = 'followscs',
-		-- ]]]
+    -- Set intelligent case insensitivity when searching [[[
+    ignorecase = true,
+    smartcase = true,
+    tagcase = 'followscs',
+    -- ]]]
 
-		-- Configure indentation handling [[[
-		-- Use current line's indentation when starting a new line
-		autoindent = true,
+    -- Configure indentation handling [[[
+    -- Use current line's indentation when starting a new line
+    autoindent = true,
 
-		-- Make autoindent more intelligent
-		smartindent = true,
+    -- Make autoindent more intelligent
+    smartindent = true,
 
-		-- Let backspace and tab intelligently treat expanded tabs like tabs
-		smarttab = true,
+    -- Let backspace and tab intelligently treat expanded tabs like tabs
+    smarttab = true,
 
-		-- Make >, <, >>, and << round to a multiple of shiftwidth
-		shiftround = true,
+    -- Make >, <, >>, and << round to a multiple of shiftwidth
+    shiftround = true,
 
-		shiftwidth = 2,
-		softtabstop = 2,
-		tabstop = 2,
-		-- ]]]
+    shiftwidth = 2,
+    softtabstop = 2,
+    tabstop = 2,
+    -- ]]]
 
-		-- Automatically read and write files appropriately [[[
-		-- If a file is changed outside vim and not changed in vim, automatically read
-		-- and display the new file contents
-		autoread = true,
+    -- Automatically read and write files appropriately [[[
+    -- If a file is changed outside vim and not changed in vim, automatically read
+    -- and display the new file contents
+    autoread = true,
 
-		-- Write modified buffers when the buffer becomes hidden
-		autowriteall = true,
-		-- ]]]
+    -- Write modified buffers when the buffer becomes hidden
+    autowriteall = true,
+    -- ]]]
 
-		-- Configure folding [[[
-		-- Needed for nvim-ufo to properly manage folding
-		foldcolumn = '1',
-		foldlevel = 99,
-		foldlevelstart = 99,
-		foldenable = true,
-		foldmethod = 'manual',
-		-- ]]]
+    -- Configure folding [[[
+    -- Needed for nvim-ufo to properly manage folding
+    foldcolumn = '1',
+    foldlevel = 99,
+    foldlevelstart = 99,
+    foldenable = true,
+    foldmethod = 'manual',
+    -- ]]]
 
-		-- Configure search match highlighting [[[
-		-- Highlight all search matches
-		hlsearch = true,
+    -- Configure search match highlighting [[[
+    -- Highlight all search matches
+    hlsearch = true,
 
-		-- Highlight search matches while typing
-		incsearch = true,
-		-- ]]]
+    -- Highlight search matches while typing
+    incsearch = true,
+    -- ]]]
 
-		-- Configure list characters display (list and listchars) [[[
-		list = true,
+    -- Configure list characters display (list and listchars) [[[
+    list = true,
 
-		-- Configure splits [[[
-		-- Open splits below the current one by default
-		splitbelow = true,
+    -- Configure splits [[[
+    -- Open splits below the current one by default
+    splitbelow = true,
 
-		-- Open splits to the right of the current one by default
-		splitright = true,
-		-- ]]]
+    -- Open splits to the right of the current one by default
+    splitright = true,
+    -- ]]]
 
-		-- Configure colors [[[
-		-- Use 24-bit RGB color in the terminal and use GUI color attributes instead of
-		-- cterm attributes
-		termguicolors = true,
-		-- ]]]
+    -- Configure colors [[[
+    -- Use 24-bit RGB color in the terminal and use GUI color attributes instead of
+    -- cterm attributes
+    termguicolors = true,
+    -- ]]]
 
-		-- Configure the status area [[[
-		-- Use 2 lines for the command area at the bottom of the screen
-		cmdheight = 2,
+    -- Configure the status area [[[
+    -- Use 2 lines for the command area at the bottom of the screen
+    cmdheight = 2,
 
-		-- Always show the status line
-		laststatus = 2,
+    -- Always show the global status line
+    laststatus = 3,
 
-		-- Don't show the mode, the airline extension will handle that
-		showmode = false,
-		-- ]]]
+    -- Don't show the mode, the airline extension will handle that
+    showmode = false,
+    -- ]]]
 
-		-- Set display appropriate for dark backgrounds
-		background = 'dark',
+    -- Set display appropriate for dark backgrounds
+    background = 'dark',
 
-		-- Highlight the column following 'textwidth'
-		colorcolumn = '+1',
+    -- Highlight the column following 'textwidth'
+    colorcolumn = '+1',
 
-		-- Concealed text is hidden unless there's a custom replacement character
-		conceallevel = 2,
+    -- Concealed text is hidden unless there's a custom replacement character
+    conceallevel = 2,
 
-		-- Confirm instead of failing operations
-		confirm = true,
+    -- Confirm instead of failing operations
+    confirm = true,
 
-		-- Highlight the line with the cursor
-		cursorline = true,
+    -- Highlight the line with the cursor
+    cursorline = true,
 
-		-- Hide unloaded buffers instead of trying to close them
-		hidden = true,
+    -- Hide unloaded buffers instead of trying to close them
+    hidden = true,
 
-		-- Do not insert two spaces after punctuation when joining lines
-		joinspaces = false,
+    -- Do not insert two spaces after punctuation when joining lines
+    joinspaces = false,
 
-		-- Don't redraw while executing macros, commands, etc.
-		lazyredraw = true,
+    -- Don't redraw while executing macros, commands, etc.
+    lazyredraw = true,
 
-		-- Intelligently break long lines
-		linebreak = true,
+    -- Intelligently break long lines
+    linebreak = true,
 
-		-- Maximum number of items in a pop-up menu
-		pumheight = 10,
+    -- Maximum number of items in a pop-up menu
+    pumheight = 10,
 
-		-- Number of lines above and below the cursor to keep visible
-		scrolloff = 3,
+    -- Number of lines above and below the cursor to keep visible
+    scrolloff = 3,
 
-		-- Show this character at the start of lines that have been wrapped
-		showbreak = '↪',
+    -- Show this character at the start of lines that have been wrapped
+    showbreak = '↪',
 
-		-- When a bracket is inserted, briefly show the matching bracket if it's
-		-- visible
-		showmatch = true,
+    -- When a bracket is inserted, briefly show the matching bracket if it's
+    -- visible
+    showmatch = true,
 
-		-- Keep the cursor in the current column if possible
-		startofline = false,
+    -- Keep the cursor in the current column if possible
+    startofline = false,
 
-		-- Maximum width of text
-		textwidth = 80,
+    -- Maximum width of text
+    textwidth = 80,
 
-		-- Wait less time for multi-character mappings
-		timeoutlen = 500,
+    -- Wait less time for multi-character mappings
+    timeoutlen = 500,
 
-		-- Milliseconds until CursorHold is triggered
-		updatetime = 100,
+    -- Milliseconds until CursorHold is triggered
+    updatetime = 100,
 
-		-- Do not write a viminfo file
-		viminfofile = 'NONE',
+    -- Do not write a viminfo file
+    viminfofile = 'NONE',
 
-		-- Allow a cursor to be placed where there's no character in Visual mode
-		virtualedit = 'block',
+    -- Allow a cursor to be placed where there's no character in Visual mode
+    virtualedit = 'block',
 
-		mouse = '',
-		number = true,
-		relativenumber = true,
-		signcolumn = "auto:3"
-	},
-	g = {
-		mapleader = '`',
+    mouse = '',
+    number = true,
+    relativenumber = true,
+    signcolumn = "auto:3"
+  },
+  g = {
+    mapleader = '`',
 
-		-- Turn off things we don't want loaded by default [[[
-		-- Do not load default menus
-		did_install_default_menus = 1,
+    -- Turn off things we don't want loaded by default [[[
+    -- Do not load default menus
+    did_install_default_menus = 1,
 
-		indent_guides_enable_on_vim_startup = 1,
+    indent_guides_enable_on_vim_startup = 1,
 
-		-- Do not load netrw
-		loaded_netrw = 1,
-		loaded_netrwPlugin = 1,
+    -- Do not load netrw
+    loaded_netrw = 1,
+    loaded_netrwPlugin = 1,
 
-		-- Do not load tohtml.vim
-		loaded_2html_plugin = 1,
+    -- Do not load tohtml.vim
+    loaded_2html_plugin = 1,
 
-		-- Do not load zipPlugin.vim, gzip.vim and tarPlugin.vim
-		loaded_zipPlugin = 1,
-		loaded_gzip = 1,
-		loaded_tarPlugin = 1,
+    -- Do not load zipPlugin.vim, gzip.vim and tarPlugin.vim
+    loaded_zipPlugin = 1,
+    loaded_gzip = 1,
+    loaded_tarPlugin = 1,
 
-		-- Do not load tutor mode
-		loaded_tutor_mode_plugin = 1,
+    -- Do not load tutor mode
+    loaded_tutor_mode_plugin = 1,
 
-		-- Disable the Python2 provider
-		loaded_python_provider = 1,
+    -- Disable the Python2 provider
+    loaded_python_provider = 1,
 
-		-- Skip the Python2 host provider check
-		python_host_skip_check = 1,
+    -- Skip the Python2 host provider check
+    python_host_skip_check = 1,
 
-		python_version = 'python3',
+    python_version = 'python3',
 
-		-- Remove all legacy commands from NeoTree
-		neo_tree_remove_legacy_commands = 1,
-		-- ]]]
+    -- Remove all legacy commands from NeoTree
+    neo_tree_remove_legacy_commands = 1,
+    -- ]]]
 
-		html_indent_script1 = 'inc',
-		html_indent_style1 = 'inc',
+    html_indent_script1 = 'inc',
+    html_indent_style1 = 'inc',
 
-		-- Enable vim-javascript highlighting for JSDocs
-		javascript_plugin_jsdoc = 1,
-		-- Enable vim-javascript highlighting for Flow
-		javascript_plugin_flow = 1,
+    -- Enable vim-javascript highlighting for JSDocs
+    javascript_plugin_jsdoc = 1,
+    -- Enable vim-javascript highlighting for Flow
+    javascript_plugin_flow = 1,
 
-		latex_to_unicode_auto = 1,
+    latex_to_unicode_auto = 1,
 
-		python_highlight_all = 1,
-		python_highlight_file_headers_as_comments = 1,
+    python_highlight_all = 1,
+    python_highlight_file_headers_as_comments = 1,
 
-		symbols_outline = {
-			auto_preview = false,
-		},
+    symbols_outline = {
+      auto_preview = false,
+    },
 
-		tex_flavor = 'xelatex',
-		Tex_CompileRule_pdf = 'xelatex --interaction=nonstopmode $*',
+    tex_flavor = 'xelatex',
+    Tex_CompileRule_pdf = 'xelatex --interaction=nonstopmode $*',
 
-		tokyonight_style = 'night',
-		tokyonight_sidebars = {
-			'qf', 'vista_kind', 'terminal', 'packer', 'NvimTree', 'Fern', 'Tagbar', 'Outline',
-		},
+    tokyonight_style = 'night',
+    tokyonight_sidebars = {
+      'qf', 'vista_kind', 'terminal', 'packer', 'NvimTree', 'Fern', 'Tagbar', 'Outline',
+    },
 
-		vim_indent_cont = vim.o.shiftwidth,
+    vim_indent_cont = vim.o.shiftwidth,
 
-		vim_markdown_autowrite = 1,
-		vim_markdown_follow_anchor = 1,
-		vim_markdown_no_extensions_in_markdown = 1,
-		vim_markdown_math = 1,
-		tex_conceal = '',
-		vim_markdown_conceal = 0,
-		vim_markdown_frontmatter = 1,
-		vim_markdown_toml_frontmatter = 1,
-		vim_markdown_json_frontmatter = 1,
-		vim_markdown_strikethrough = 1,
-		vim_markdown_new_list_item_indent = 2,
-		vim_markdown_edit_url_in = 'tab',
+    vim_markdown_autowrite = 1,
+    vim_markdown_follow_anchor = 1,
+    vim_markdown_no_extensions_in_markdown = 1,
+    vim_markdown_math = 1,
+    tex_conceal = '',
+    vim_markdown_conceal = 0,
+    vim_markdown_frontmatter = 1,
+    vim_markdown_toml_frontmatter = 1,
+    vim_markdown_json_frontmatter = 1,
+    vim_markdown_strikethrough = 1,
+    vim_markdown_new_list_item_indent = 2,
+    vim_markdown_edit_url_in = 'tab',
 
-		xdg_config_home = os.getenv('XDG_CONFIG_HOME') ~= nil and os.getenv('XDG_CONFIG_HOME') or
-				os.getenv('HOME') .. '/.config'
-	},
+    xdg_config_home = os.getenv('XDG_CONFIG_HOME') ~= nil and os.getenv('XDG_CONFIG_HOME') or
+        os.getenv('HOME') .. '/.config'
+  },
 }
 
 return options
