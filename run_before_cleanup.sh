6f0f4b7135d19a143b7ebf80e7a7b9adf3882a60
Joel Goguen <jgoguen@users.noreply.github.com>
2022-12-20 14:58:04 -0500

    Create tmux config directory before trying to use it

--- run_before_cleanup.sh
+++ run_before_cleanup.sh
@@ -8,6 +8,7 @@ if [ -f "${HOME}/.tmux.conf" ]; then
 	if [ -f "${XDG_CONFIG_HOME:-${HOME}/.config}/tmux/tmux.conf" ]; then
 		printf '%s already exists, please merge %s into it\n' "${XDG_CONFIG_HOME:-${HOME}/.config}/tmux/tmux.conf" "${HOME}/.tmux.conf"
 	else
+		test -d "${XDG_CONFIG_HOME:-${HOME}/.config}/tmux" || mkdir -p "${XDG_CONFIG_HOME:-${HOME}/.config}/tmux"
 		mv "${HOME}/.tmux.conf" "${XDG_CONFIG_HOME:-${HOME}/.config}/tmux/tmux.conf"
 	fi
 fi
