43232f495bd1aa163f8ed9c65ec3a16870ac94fa
Joel Goguen <jgoguen@users.noreply.github.com>
2022-12-19 22:45:15 -0500

    Enable extra regex highlighting for Markdown documents

--- dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/treesitter.lua
+++ dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/treesitter.lua
@@ -1,13 +1,16 @@
 return {
-	ensure_installed = {
-		"bash", "c", "c_sharp", "comment", "cpp", "css", "dockerfile", "dot",
-		"go", "gomod", "html", "http", "javascript", "jsdoc", "json", "json5",
-		"jsonc", "latex", "lua", "make", "markdown", "php", "python", "ruby",
-		"scss", "toml", "vim", "yaml",
-	},
-	highlight = { enable = true },
-	rainbow = {
-		extended_mode = true,
-	},
-	textobjects = { enable = true },
+  ensure_installed = {
+    "bash", "c", "c_sharp", "comment", "cpp", "css", "dockerfile", "dot",
+    "go", "gomod", "html", "http", "javascript", "jsdoc", "json", "json5",
+    "jsonc", "latex", "lua", "make", "markdown", "php", "python", "ruby",
+    "scss", "toml", "vim", "yaml",
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = { "markdown", "pandoc.markdown" },
+  },
+  rainbow = {
+    extended_mode = true,
+  },
+  textobjects = { enable = true },
 }
