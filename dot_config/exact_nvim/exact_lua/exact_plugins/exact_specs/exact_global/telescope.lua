99036ca551f6305ffcb5be882ae2a8fe2ebd6dad
Joel Goguen <jgoguen@jgoguen.ca>
2023-08-24 17:17:47 -0400

    Configure fzf Telescope extension

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_specs/exact_global/telescope.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_specs/exact_global/telescope.lua
@@ -91,6 +91,12 @@ function M.config()
 				path = '%:p:h',
 				select_buffer = true,
 			},
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = 'smart_case',
+			},
 		},
 	})
 
