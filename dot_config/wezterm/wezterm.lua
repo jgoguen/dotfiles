1b0e78d091f1284ecafd9aa5b2233b68bde9b54a
Joel Goguen <jgoguen@jgoguen.ca>
2023-09-09 15:04:59 -0400

    Use new Wezterm config builder

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,36 +1,41 @@
 local wezterm = require 'wezterm'
 local has_site, site_config = pcall(require, 'wezterm_local')
 
-local config = {
-	automatically_reload_config = true,
-	bold_brightens_ansi_colors = true,
-	clean_exit_codes = { 130 },
-	color_scheme = 'tokyonight_day',
-	font = wezterm.font_with_fallback({
-		'JetBrainsMono Nerd Font',
-		'FiraCode Nerd Font',
-		'Fira Code',
-		'Noto Sans CJK JP',
-		'Symbols Nerd Font',
-	}),
-	font_size = 13,
-	harfbuzz_features = { 'ss02', 'ss03', 'ss05', 'ss07', 'cv22', 'cv24', 'cv25', 'cv26', 'cv32' },
-	hyperlink_rules = {
-		-- Include the default since we're overriding
-		{
-			regex = '\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b',
-			format = '$0',
-		},
+local config = {}
+
+-- In newer versions of wezterm, use the config builder for better errors
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.automatically_reload_config = true
+config.bold_brightens_ansi_colors = true
+config.clean_exit_codes = { 130 }
+config.color_scheme = 'tokyonight_day'
+config.font = wezterm.font_with_fallback({
+	'JetBrainsMono Nerd Font',
+	'FiraCode Nerd Font',
+	'Fira Code',
+	'Noto Sans CJK JP',
+	'Symbols Nerd Font',
+})
+config.font_size = 13
+config.harfbuzz_features = { 'ss02', 'ss03', 'ss05', 'ss07', 'cv22', 'cv24', 'cv25', 'cv26', 'cv32' }
+config.hyperlink_rules = {
+	-- Include the default since we're overriding
+	{
+		regex = '\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b',
+		format = '$0',
 	},
-	keys = {
-		{ key = 'PageUp', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = -1 } },
-		{ key = 'LeftArrow', mods = 'CMD', action = wezterm.action { ActivateTabRelative = -1 } },
-		{ key = 'PageDown', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = 1 } },
-		{ key = 'RightArrow', mods = 'CMD', action = wezterm.action { ActivateTabRelative = 1 } },
-	},
-	selection_word_boundary = ' \t\n{}[]()"\'`:',
-	window_close_confirmation = 'NeverPrompt',
 }
+config.keys = {
+	{ key = 'PageUp', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = -1 } },
+	{ key = 'LeftArrow', mods = 'CMD', action = wezterm.action { ActivateTabRelative = -1 } },
+	{ key = 'PageDown', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = 1 } },
+	{ key = 'RightArrow', mods = 'CMD', action = wezterm.action { ActivateTabRelative = 1 } },
+}
+config.selection_word_boundary = ' \t\n{}[]()"\'`:'
+config.window_close_confirmation = 'NeverPrompt'
 
 if has_site then
 	config = site_config.update_config(config)
