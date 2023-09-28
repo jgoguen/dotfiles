dcb1a09a745a335f5f8c6bbceca9a18a0cd3df57
Joel Goguen <jgoguen@jgoguen.ca>
2023-09-28 11:30:11 -0400

    Make lualine show the filetype name

--- dot_config/exact_nvim/exact_lua/exact_plugins/lualine.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/lualine.lua
@@ -4,12 +4,16 @@ local M = {
 	'nvim-lualine/lualine.nvim',
 	opts = function(_, opts)
 		opts.options.disabled_filetypes = require('utils').excluded_filetypes
+
+		table.insert(opts.sections.lualine_x, { 'filetype' })
+
 		opts.sections.lualine_y = {
 			{ 'progress', separator = ' ', padding = { left = 1, right = 0 } },
 			{ '%l/%L' },
 			{ '%v' },
 			{ '%B' },
 		}
+
 		table.insert(opts.sections.lualine_z, 1, function()
 			local f = io.popen('/bin/hostname -s')
 			if f == nil then
