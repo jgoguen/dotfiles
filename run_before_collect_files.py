fb7016ef457b3b6084a1655ea557c89efbd2eea9
Joel Goguen <jgoguen@jgoguen.ca>
2023-11-06 21:17:47 -0500

    Add debug logging to file collector

--- run_before_collect_files.py
+++ run_before_collect_files.py
@@ -3,12 +3,13 @@
 # vim: set foldmarker=[[[,]]] foldmethod=marker foldlevel=0:
 
 import json
+import logging
 import os
 import subprocess
 import sys
 
 
-OP_BIN_PATHS = ["/usr/local/bin/op", "/usr/bin/op"]
+OP_BIN_PATHS = ["/opt/homebrew/bin/op", "/usr/local/bin/op", "/usr/bin/op"]
 OP_BIN = [
     p for p in OP_BIN_PATHS if os.path.exists(p)
 ][0]
@@ -19,6 +20,8 @@ def main() -> int:
         print(f"{OP_BIN} does not exist, cannot gather files", file=sys.stderr)
         return 1
 
+    logging.debug("Using op location: %s", OP_BIN)
+
     proc = subprocess.run(
         [OP_BIN, "document", "list", "--format", "json"],
         capture_output=True,
@@ -27,11 +30,13 @@ def main() -> int:
 
     op_docs = json.loads(proc.stdout)
     files = {}
+    logging.debug("Loaded 1Password documents")
 
     for op_doc in op_docs:
         for tag in op_doc.get("tags", []):
             if tag.startswith("chezmoi_file:"):
                 files[op_doc["id"]] = tag.split(":", 2)[1]
+                logging.debug("Found file to install at %s", files[op_doc["id"]])
                 break
 
     excludes = []
@@ -39,6 +44,8 @@ def main() -> int:
         ["chezmoi", "source-path"], capture_output=True, check=True
     )
     source_base = chezmoi_proc.stdout.decode("UTF-8").strip()
+    logging.debug("Chezmoi source path: %s", source_base)
+
     for uuid in files:
         dirname = os.path.expanduser(os.path.dirname(files[uuid]))
         basename = os.path.basename(files[uuid])
@@ -47,6 +54,12 @@ def main() -> int:
         )
         source_dir = proc.stdout.decode("UTF-8").strip()
         source_path = os.path.join(source_dir, basename)
+
+        logging.debug(
+            "Installing file %s to Chezmoi source path %s",
+            basename,
+            source_path,
+        )
 
         if os.path.isfile(source_path):
             os.unlink(source_path)
@@ -58,10 +71,16 @@ def main() -> int:
         excludes.append(os.path.relpath(source_path, source_base))
 
     with open(os.path.join(source_base, ".git", "info", "exclude"), "w") as f:
+        logging.debug("Writing files to git excludes: %s", excludes)
         f.write("\n".join(excludes))
 
     return 0
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO if os.environ.get("DEBUG") is None else logging.DEBUG,
+        style="{",
+        format="{asctime} [{levelname}] [{filename}:{lineno}] {message}",
+    )
     sys.exit(main())
