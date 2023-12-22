f91a0f32eed4842481795404bb7f9152415ed5d3
Joel Goguen <jgoguen@jgoguen.ca>
2023-12-22 18:03:47 -0500

    Update calibre config

---
+++ dot_config/calibre/conversion/kepub_output.py
@@ -0,0 +1,20 @@
+json:{
+  "dont_split_on_page_breaks": false,
+  "flow_size": 260,
+  "no_default_epub_cover": false,
+  "no_svg_cover": false,
+  "epub_inline_toc": false,
+  "epub_toc_at_end": false,
+  "toc_title": null,
+  "preserve_cover_aspect_ratio": false,
+  "epub_flatten": false,
+  "epub_version": "3",
+  "epub_max_image_size": "none",
+  "kepub_hyphenate": true,
+  "kepub_clean_markup": true,
+  "kepub_disable_hyphenation": false,
+  "kepub_hyphenate_chars": 6,
+  "kepub_hyphenate_chars_before": 3,
+  "kepub_hyphenate_chars_after": 3,
+  "kepub_hyphenate_limit_lines": 2
+}
