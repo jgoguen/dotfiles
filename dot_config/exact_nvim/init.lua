1cfba88cf674e0d0604320f7a9010726b87cf119
Joel Goguen <jgoguen@jgoguen.ca>
2024-01-30 22:43:37 -0500

    Guard against null globpath, add helptags

--- dot_config/exact_nvim/init.lua
+++ dot_config/exact_nvim/init.lua
@@ -8,6 +8,10 @@ vim.g.xdg_data_home = XDG_DATA_HOME
 require('config.lazy')
 
 local settings = vim.fn.globpath(vim.o.runtimepath, 'settings/*.vim', 0, 1)
-for _, f in ipairs(settings) do
-	vim.cmd('source ' .. f)
+if settings ~= nil then
+	for _, f in ipairs(settings) do
+		vim.cmd('source ' .. f)
+	end
 end
+
+vim.cmd('helptags ALL')
