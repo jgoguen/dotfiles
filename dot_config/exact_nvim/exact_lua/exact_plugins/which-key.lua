3b50c3cc2f59722061678fcb4e1543e47b8d45fb
Joel Goguen <jgoguen@jgoguen.ca>
2024-04-15 09:36:27 -0400

    Fix which-key option setup

--- dot_config/exact_nvim/exact_lua/exact_plugins/which-key.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/which-key.lua
@@ -3,6 +3,9 @@
 local M = {
 	'folke/which-key.nvim',
 	opts = function(_, opts)
+		opts['plugins'] = opts['plugins'] or {}
+		opts['window'] = opts['window'] or {}
+
 		opts['plugins']['marks'] = false
 		opts['window']['border'] = 'double'
 	end,
