11df437a537d25ac4f0c1f1640603d593c7d830e
Joel Goguen <jgoguen@jgoguen.ca>
2024-09-10 19:34:29 -0400

    Fix getting CWD for python venv

--- dot_config/exact_nvim/exact_after/exact_ftplugin/python.lua
+++ dot_config/exact_nvim/exact_after/exact_ftplugin/python.lua
@@ -3,8 +3,6 @@
 vim.opt_local.includeexpr = 'import#PythonImport(v:fname)'
 vim.opt_local.spell = false
 
-if vim.fn.filereadable('venv/bin/python3') == 1 then
-	-- TODO: This won't work on Windows.
-	-- Maybe os.getenv("CD"), or io.popen("cd"):read("*l")
-	vim.g.python3_host_prog = os.getenv('PWD') .. '/venv/bin/python3'
+if vim.fn.filereadable(vim.fn.getcwd() .. '/venv/bin/python3') == 1 then
+	vim.g.python3_host_prog = vim.fn.getcwd() .. '/venv/bin/python3'
 end
