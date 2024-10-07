f9a7e435acd8806e282bdfd0a47cd028d4da3aa3
Joel Goguen <jgoguen@jgoguen.ca>
2024-10-06 22:14:53 -0400

    Enable lazyvim git language plugin

--- dot_config/exact_nvim/exact_lua/exact_config/lazy.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/lazy.lua
@@ -30,6 +30,7 @@ local lazy_specs = {
 	{ import = 'lazyvim.plugins.extras.editor.telescope' },
 	{ import = 'lazyvim.plugins.extras.formatting.prettier' },
 	{ import = 'lazyvim.plugins.extras.lang.docker' },
+	{ import = 'lazyvim.plugins.extras.lang.git' },
 	{ import = 'lazyvim.plugins.extras.lang.go' },
 	{ import = 'lazyvim.plugins.extras.lang.java' },
 	{ import = 'lazyvim.plugins.extras.lang.json' },
