4b24ca38e9771f1724939d7063aa6b998ceba70c
Joel Goguen <jgoguen@jgoguen.ca>
2025-01-17 17:12:56 -0500

    feat(treesitter): Install Tree Sitter playground
    Shows the Tree Sitter parsed tree based on cursor position

--- dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
@@ -13,6 +13,7 @@ local M = {
 	dependencies = {
 		'RRethy/nvim-treesitter-endwise',
 		'ngalaiko/tree-sitter-go-template',
+		'nvim-treesitter/playground',
 		'windwp/nvim-ts-autotag',
 	},
 	opts = function(_, opts)
@@ -27,6 +28,10 @@ local M = {
 		for _, p in ipairs(extra_parsers) do
 			table.insert(opts.ensure_installed, p)
 		end
+
+		opts['playground'] = {
+			enable = true,
+		}
 	end,
 }
 
