52e94858017ec2c4ede74d5c56872fb957bdf625
Joel Goguen <jgoguen@jgoguen.ca>
2025-01-19 16:34:22 -0500

    config(nvim): Add chezmoi extra for nvim

--- dot_config/exact_nvim/exact_lua/exact_config/lazy.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/lazy.lua
@@ -40,6 +40,7 @@ local lazy_specs = {
 	{ import = 'lazyvim.plugins.extras.lang.yaml' },
 	{ import = 'lazyvim.plugins.extras.test.core' },
 	{ import = 'lazyvim.plugins.extras.ui.treesitter-context' },
+	{ import = 'lazyvim.plugins.extras.util.chezmoi' },
 	-- import/override with your plugins
 	{ import = 'plugins' },
 	{ import = 'plugins.lang' },
