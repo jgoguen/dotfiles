3280f278cab522a267770a6f4430cedd9443e038
Joel Goguen <jgoguen@jgoguen.ca>
2025-02-26 09:27:51 -0500

    feat(nvim.blink): Allow easy completion item selection

--- dot_config/exact_nvim/exact_lua/exact_plugins/blink.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/blink.lua
@@ -6,13 +6,84 @@ local M = {
 	{
 		'saghen/blink.cmp',
 		opts = function(_, opts)
+			local new_opts = {
+				completion = {
+					ghost_text = {
+						enabled = true,
+					},
+					menu = {
+						draw = {
+							columns = {
+								{ 'item_idx' },
+								{ 'kind_icon', 'kind' },
+								{ 'label', 'label_description' },
+							},
+						},
+					},
+				},
+				keymap = {
+					['<A-1>'] = {
+						function(cmp)
+							cmp.accept({ index = 1 })
+						end,
+					},
+					['<A-2>'] = {
+						function(cmp)
+							cmp.accept({ index = 2 })
+						end,
+					},
+					['<A-3>'] = {
+						function(cmp)
+							cmp.accept({ index = 3 })
+						end,
+					},
+					['<A-4>'] = {
+						function(cmp)
+							cmp.accept({ index = 4 })
+						end,
+					},
+					['<A-5>'] = {
+						function(cmp)
+							cmp.accept({ index = 5 })
+						end,
+					},
+					['<A-6>'] = {
+						function(cmp)
+							cmp.accept({ index = 6 })
+						end,
+					},
+					['<A-7>'] = {
+						function(cmp)
+							cmp.accept({ index = 7 })
+						end,
+					},
+					['<A-8>'] = {
+						function(cmp)
+							cmp.accept({ index = 8 })
+						end,
+					},
+					['<A-9>'] = {
+						function(cmp)
+							cmp.accept({ index = 9 })
+						end,
+					},
+					['<A-0>'] = {
+						function(cmp)
+							cmp.accept({ index = 10 })
+						end,
+					},
+				},
+				signature = {
+					enabled = true,
+				},
+			}
+
+			opts = vim.tbl_deep_extend('force', opts, new_opts)
 			opts.enabled = function()
-				if
+				return not (
 					vim.tbl_contains(Utils.excluded_buftypes, vim.bo.buftype)
 					or vim.tbl_contains(Utils.excluded_filetypes, vim.bo.filetype)
-				then
-					return false
-				end
+				)
 			end
 		end,
 	},
