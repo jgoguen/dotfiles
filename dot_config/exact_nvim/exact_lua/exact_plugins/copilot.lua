64f32abc2f9024f648f2b783ead5bde90042fb0d
Joel Goguen <jgoguen@jgoguen.ca>
2025-08-25 15:36:04 -0400

    fix(nvim): Do not use copilot in norg files

--- dot_config/exact_nvim/exact_lua/exact_plugins/copilot.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/copilot.lua
@@ -1,11 +1,12 @@
 local M = {
 	{
-		"zbirenbaum/copilot.lua",
+		'zbirenbaum/copilot.lua',
 		opts = {
 			root_dir = function()
 				return LazyVim.root() or LazyVim.root.cwd()
 			end,
 			filetypes = {
+				norg = false,
 				yaml = true,
 			},
 		},
